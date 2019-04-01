3efdb24db6c4cf0b05a51955bd56d75648e075e9
yesid1010 <sanchezyesid72@gmail.com>
2019-04-01 13:35:49 -0500

    all

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,62 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController } from 'ionic-angular';
+
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  numero:string='';
+  tablas = [];
+  divisor = [];
+  opcion : boolean = false;
+  constructor(public navCtrl: NavController, public alert: AlertController) {
+    
   }
 
+  tabla(numero){
+    if(numero == ''){
+      this.alerta();
+    }else{
+      let resul;
+      for(let i = 0 ; i <= 10;i++){
+          resul = numero * i;
+          this.tablas[i] = resul;
+      } 
+      console.log(this.tablas);
+      this.opcion = false;
+    }
+  }
+
+  divisores(numero){
+    if(numero == ''){
+      this.alerta();
+    }else{
+      let cont = 0;
+      for(let a = 0 ; a <= this.divisor.length;a++){
+        this.divisor.shift()
+      }
+      for(let i = 1 ; i<=numero;i++){
+          if(numero % i == 0){
+            this.divisor[cont] = i;
+            cont++;
+          }
+
+      }
+      this.opcion= true;
+      
+    }
+  }
+
+  alerta(){
+    let al = this.alert.create({
+      title : 'Debes escribir un numero',
+      buttons : [{
+        text : 'Entendido',
+      }]
+    }); 
+    al.present();
+  }
 }
